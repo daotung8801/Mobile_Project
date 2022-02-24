f984ec38a89438bec55cee2454e53fc51de97688
daotung8801 <daoxuantung8801@gmail.com>
2022-02-24 16:46:06 +0700

    Task 7: Multiple Child Widgets demo

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello World Demo Application',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyButton(),
+      title: 'Flutter Demo', theme: ThemeData(
+      primarySwatch: Colors.blue,),
+      home: MyHomePage(title: 'Product layout demo home page'),
     );
   }
 }
@@ -19,41 +17,78 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(this.title),),
-      body: Center( child: Icon(Icons.email)),
+        appBar: AppBar(title:Text("Product Listing")),
+        body: ListView(
+          shrinkWrap: true, padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+          children: <Widget> [
+            ProductBox(
+                name: "iPhone",
+                description: "iPhone is the stylist phone ever",
+                price: 1000,
+                image: "iphone.jpg"
+            ),
+            ProductBox(
+                name: "Pixel",
+                description: "Pixel is the most featureful phone ever",
+                price: 800,
+                image: "pixel.jpg"
+            ),
+            ProductBox(
+                name: "Laptop",
+                description: "Laptop is most productive development tool",
+                price: 2000,
+                image: "laptop.jpg"
+            ),
+            ProductBox(
+                name: "Tablet",
+                description: "Tablet is the most useful device ever for meeting",
+                price: 1500,
+                image: "tablet.jpg"
+            ),
+            ProductBox(
+                name: "Pendrive",
+                description: "Pendrive is useful storage medium",
+                price: 100,
+                image: "pendrive.jpg"
+            ),
+            ProductBox(
+                name: "Floppy Drive",
+                description: "Floppy drive is useful rescue storage medium",
+                price: 20,
+                image: "floppydisk.jpg"
+            ),
+          ],
+        )
     );
   }
 }
-
-class MyButton extends StatelessWidget {
-  MyButton({Key? key}) : super(key: key);
-  @override
+class ProductBox extends StatelessWidget {
+  ProductBox({Key? key, required this.name, required this.description, required this.price, required this.image})
+      : super(key: key);
+  final String name;
+  final String description;
+  final int price;
+  final String image;
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        border: Border(
-          top: BorderSide(width: 1.0, color: Colors.red),
-          left: BorderSide(width: 1.0, color: Colors.red),
-          right: BorderSide(width: 1.0, color: Colors.red),
-          bottom: BorderSide(width: 1.0, color: Colors.red),
-        ),
-      ),
-      child: Container(
-        padding: const
-        EdgeInsets.symmetric(horizontal: 20.0, vertical: 2.0),
-        decoration: const BoxDecoration(
-          border: Border(
-            top: BorderSide(width: 1.0, color: Colors.red),
-            left: BorderSide(width: 1.0, color: Colors.red),
-            right: BorderSide(width: 1.0, color: Colors.red),
-            bottom: BorderSide(width: 1.0, color: Colors.red),
-          ),
-          color: Colors.white,
-        ),
-        child: Center(
-          child: const Text('Button demo', style: TextStyle(color: Colors.black)),
-      ),
-      ),
+        padding: EdgeInsets.all(2), height: 120, child: Card(
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
+          Image.asset("assets/appimages/" +image), Expanded(
+              child: Container(
+                  padding: EdgeInsets.all(5), child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(this.name, style: TextStyle(fontWeight:
+                  FontWeight.bold)), Text(this.description),
+                  Text("Price: " + this.price.toString()),
+                ],
+              )
+              )
+          )
+        ]
+        )
+    )
     );
   }
 }
