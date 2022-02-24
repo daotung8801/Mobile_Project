3486b74e0ac51d654ed07de08f9c4f76b517a9f3
daotung8801 <daoxuantung8801@gmail.com>
2022-02-24 17:11:22 +0700

    Task 8: Gesture demo

--- lib/main.dart
+++ lib/main.dart
@@ -5,90 +5,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo', theme: ThemeData(
-      primarySwatch: Colors.blue,),
-      home: MyHomePage(title: 'Product layout demo home page'),
+      title: 'Hello World Demo Application',
+      theme: ThemeData( primarySwatch: Colors.blue,),
+      home: MyHomePage(title: 'Home page'),
     );
   }
 }
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
+// user defined function
+  void _showDialog(BuildContext context) {
+// user defined function void _showDialog(BuildContext context) {
+// flutter defined function
+    showDialog(
+      context: context, builder: (BuildContext context) {
+// return object of type Dialog
+      return AlertDialog(
+        title: new Text("Message"),
+        content: new Text("Hello World from Viet Nam !!"),
+        actions: <Widget>[
+          new FlatButton(
+            child: new Text("Close"),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title:Text("Product Listing")),
-        body: ListView(
-          shrinkWrap: true, padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
-          children: <Widget> [
-            ProductBox(
-                name: "iPhone",
-                description: "iPhone is the stylist phone ever",
-                price: 1000,
-                image: "iphone.jpg"
-            ),
-            ProductBox(
-                name: "Pixel",
-                description: "Pixel is the most featureful phone ever",
-                price: 800,
-                image: "pixel.jpg"
-            ),
-            ProductBox(
-                name: "Laptop",
-                description: "Laptop is most productive development tool",
-                price: 2000,
-                image: "laptop.jpg"
-            ),
-            ProductBox(
-                name: "Tablet",
-                description: "Tablet is the most useful device ever for meeting",
-                price: 1500,
-                image: "tablet.jpg"
-            ),
-            ProductBox(
-                name: "Pendrive",
-                description: "Pendrive is useful storage medium",
-                price: 100,
-                image: "pendrive.jpg"
-            ),
-            ProductBox(
-                name: "Floppy Drive",
-                description: "Floppy drive is useful rescue storage medium",
-                price: 20,
-                image: "floppydisk.jpg"
-            ),
-          ],
-        )
-    );
-  }
-}
-class ProductBox extends StatelessWidget {
-  ProductBox({Key? key, required this.name, required this.description, required this.price, required this.image})
-      : super(key: key);
-  final String name;
-  final String description;
-  final int price;
-  final String image;
-  Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.all(2), height: 120, child: Card(
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
-          Image.asset("assets/appimages/" +image), Expanded(
-              child: Container(
-                  padding: EdgeInsets.all(5), child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text(this.name, style: TextStyle(fontWeight:
-                  FontWeight.bold)), Text(this.description),
-                  Text("Price: " + this.price.toString()),
-                ],
-              )
-              )
+      appBar: AppBar(title: Text(this.title),),
+      body: Center(
+          child: GestureDetector(
+              onTap: () {
+                _showDialog(context);
+              },
+              child: Text( 'Hello World', )
           )
-        ]
-        )
-    )
+      ),
     );
   }
 }
