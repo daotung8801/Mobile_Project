3ede8ad39ca298aaee66d38b2f318a700f0c9487
daotung8801 <daoxuantung8801@gmail.com>
2022-02-27 17:42:36 +0700

    Task13: Animation demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,276 +1,145 @@
 import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
-class Product {
-  final String name;
-  final String description;
-  final int price;
-  final String image;
-  Product(this.name, this.description, this.price, this.image);
-  static List<Product> getProducts() {
-    List<Product> items = <Product>[];
-    items.add(
-        Product(
-            "Pixel",
-            "Pixel is the most featureful phone ever",
-            800,
-            "pixel.jpg"
-        )
-    );
-    items.add(
-        Product(
-            "Laptop",
-            "Laptop is most productive development tool",
-            2000,
-            "laptop.jpg"
-        )
-    );
-    items.add(
-        Product(
-            "Tablet",
-            "Tablet is the most useful device ever for meeting",
-            1500,
-            "tablet.jpg"
-        )
-    );
-    items.add(
-        Product(
-            "Pendrive",
-            "iPhone is the stylist phone ever",
-            100,
-            "pendrive.jpg"
-        )
-    );
-    items.add(
-        Product(
-            "Floppy Disk",
-            "iPhone is the stylist phone ever",
-            20,
-            "floppydisk.jpg"
-        )
-    );
-    items.add(
-        Product(
-            "iPhone",
-            "iPhone is the stylist phone ever",
-            1000,
-            "iphone.jpg"
-        )
-    );
-    return items;
-  }
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
 }
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        duration: const Duration(seconds: 20), vsync: this);
+    animation = Tween<double>(begin: 0.0, end: 1.0).animate(controller);
+    controller.forward();
+  }
 // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    controller.forward();
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Product Navigation demo home page'),
+        title: 'Flutter Demo', theme: ThemeData(primarySwatch: Colors.blue,),
+        home: MyHomePage(title: 'Product layout demo home page', animation:
+        animation,)
     );
+  }
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
 class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+  MyHomePage({Key? key, required this.title, required this.animation}): super(key: key);
   final String title;
-  final items = Product.getProducts();
+  final Animation<double> animation;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Product Navigation")),
-        body: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              child: ProductBox(item: items[index]),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ProductPage(item: items[index]),
-                  ),
-                );
-              },
-            );
-          },
+        appBar: AppBar(title: Text("Product Listing")),
+        body: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+          children: <Widget>[
+            FadeTransition(
+                child: ProductBox(
+                    name: "iPhone",
+                    description: "iPhone is the stylist phone ever",
+                    price: 1000,
+                    image: "iphone.jpg"
+                ),
+                opacity: animation
+            ),
+            MyAnimatedWidget(
+                child: ProductBox(
+                    name: "Pixel",
+                    description: "Pixel is the most featureful phone ever",
+                    price: 800,
+                    image: "pixel.jpg"
+                ),
+                animation: animation
+            ),
+            ProductBox(
+                name: "Laptop",
+                description: "Laptop is most productive development tool",
+                price: 2000,
+                image: "laptop.jpg"
+            ),
+            ProductBox(
+                name: "Tablet",
+                description: "Tablet is the most useful device ever for meeting",
+                price: 1500,
+                image: "tablet.jpg"
+            ),
+            ProductBox(
+                name: "Pendrive",
+                description: "Pendrive is useful storage medium",
+                price: 100,
+                image: "pendrive.jpg"
+            ),
+            ProductBox(
+                name: "Floppy Drive",
+                description: "Floppy drive is useful rescue storage medium",
+                price: 20,
+                image: "floppydisk.jpg"
+            ),
+          ],
         )
-    );
-  }
-}
-class ProductPage extends StatelessWidget {
-  ProductPage({Key? key, required this.item}) : super(key: key);
-  final Product item;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(this.item.name),
-      ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(0),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Image.asset("assets/appimages/" + this.item.image,height: 100.0,width:
-                MediaQuery.of(context).size.width,),
-                Expanded(
-                    child: Container(
-                        padding: EdgeInsets.all(5),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Text(this.item.name, style: TextStyle(fontWeight:
-                            FontWeight.bold)),
-                            Text(this.item.description),
-                            Text("Price: " + this.item.price.toString()),
-                            RatingBox(),
-                          ],
-                        )
-                    )
-                )
-              ]
-          ),
-        ),
-      ),
-    );
-  }
-}
-class RatingBox extends StatefulWidget {
-  @override
-  _RatingBoxState createState() => _RatingBoxState();
-}
-class _RatingBoxState extends State<RatingBox> {
-  int _rating = 0;
-  void _setRatingAsZero() {
-    setState(() {
-      _rating = 0;
-    });
-  }
-  void _setRatingAsOne() {
-    setState(() {
-      _rating = 1;
-    });
-  }
-  void _setRatingAsTwo() {
-    setState(() {
-      _rating = 2;
-    });
-  }
-  void _setRatingAsThree() {
-    setState(() {
-      _rating = 3;
-    });
-  }
-  Widget build(BuildContext context) {
-    double _size = 20;
-    print(_rating);
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      mainAxisSize: MainAxisSize.max,
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(0),
-          child: IconButton(
-            icon: (
-                _rating >= 1 ? Icon(
-                  Icons.star,
-                  size: _size,
-                )
-                    : Icon(
-                  Icons.star_border,
-                  size: _size,
-                )
-            ),
-            color: Colors.red[500],
-            onPressed: _setRatingAsOne,
-            iconSize: _size,
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.all(0),
-          child: IconButton(
-            icon: (
-                _rating >= 2 ?
-                Icon(
-                  Icons.star,
-                  size: _size,
-                )
-                    : Icon(
-                  Icons.star_border,
-                  size: _size,
-                )
-            ),
-            color: Colors.red[500],
-            onPressed: _setRatingAsTwo,
-            iconSize: _size,
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.all(0),
-          child: IconButton(
-            icon: (
-                _rating >= 3 ?
-                Icon(
-                  Icons.star,
-                  size: _size,
-                )
-                    : Icon(
-                  Icons.star_border,
-                  size: _size,
-                )
-            ),
-            color: Colors.red[500],
-            onPressed: _setRatingAsThree,
-            iconSize: _size,
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.all(0),
-          child: GestureDetector(
-            onTap: () {
-              _setRatingAsZero();
-            },
-          child: Text( 'Clear', )
-          ),
-        ),
-      ],
     );
   }
 }
 class ProductBox extends StatelessWidget {
-  ProductBox({Key? key, required this.item}) : super(key: key);
-  final Product item;
+  ProductBox({Key? key, required this.name, required this.description, required this.price, required this.image}) :
+        super(key: key);
+  final String name;
+  final String description;
+  final int price;
+  final String image;
   Widget build(BuildContext context) {
     return Container(
         padding: EdgeInsets.all(2),
         height: 140,
         child: Card(
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Image.asset("assets/appimages/" + this.item.image),
-                Expanded(
-                    child: Container(
-                        padding: EdgeInsets.all(5),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Text(this.item.name, style: TextStyle(fontWeight:
-                            FontWeight.bold)), Text(this.item.description),
-                            Text("Price: " + this.item.price.toString()),
-                            RatingBox(),
-                          ],
-                        )
-                    )
-                )
-              ]
-          ),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Image.asset("assets/appimages/" + image),
+                  Expanded(
+                      child: Container(
+                          padding: EdgeInsets.all(5),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Text(
+                                  this.name, style: TextStyle(
+                                  fontWeight: FontWeight.bold
+                              )
+                              ),
+                              Text(this.description), Text(
+                                  "Price: " + this.price.toString()
+                              ),
+                            ],
+                          )
+                      )
+                  )
+                ]
+            )
         )
     );
   }
+}
+class MyAnimatedWidget extends StatelessWidget {
+  MyAnimatedWidget({required this.child, required this.animation});
+  final Widget child;
+  final Animation<double> animation;
+  Widget build(BuildContext context) => Center(
+    child: AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) => Container(
+          child: Opacity(opacity: animation.value, child: child),
+        ),
+        child: child
+    ),
+  );
 }
