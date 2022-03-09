bb06fd33c7df1d9a18337f1185c30398dbcf00da
daotung8801 <daoxuantung8801@gmail.com>
2022-03-09 16:47:46 +0700

    Task19: Change language demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,269 +1,106 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'Product.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart';
 
-void main() => runApp(MyApp(products: fetchProducts()));
+import 'generated/intl/messages_all.dart';
 
-List<Product> parseProducts(String responseBody) {
-  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
-  return parsed.map<Product>((json) => Product.fromMap(json)).toList();
-}
-
-Future<List<Product>> fetchProducts() async {
-  final response =
-  await http.get(Uri.parse('http://192.168.244.1:8000/products.json'));
-  if (response.statusCode == 200) {
-    return parseProducts(response.body);
-  } else {
-    throw Exception('Unable to fetch products from the REST API');
-  }
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final Future<List<Product>> products;
 
-  MyApp({Key? key, required this.products}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(
-          title: 'Product Navigation demo home page', products: products),
+      title: 'Localizations Sample App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      localizationsDelegates: [
+        CustomLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: [
+        const Locale('en', ''),
+        const Locale('es', ''),
+        const Locale('vi', ''),
+      ],
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  final Future<List<Product>> products;
 
-  MyHomePage({Key? key, required this.title, required this.products})
-      : super(key: key);
-
-  // final items = Product.getProducts();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text("Product Navigation")),
-        body: Center(
-          child: FutureBuilder<List<Product>>(
-            future: products,
-            builder: (context, snapshot) {
-              if (snapshot.hasError) print(snapshot.error);
-              return snapshot.hasData
-                  ? ProductBoxList(items: snapshot.requireData)
-                  :
-
-              // return the ListView widget :
-              Center(child: CircularProgressIndicator());
-            },
-          ),
-        ));
-  }
-}
-
-class ProductBoxList extends StatelessWidget {
-  final List<Product> items;
-
-  ProductBoxList({Key? key, required this.items});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          child: ProductBox(item: items[index]),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => ProductPage(item: items[index]),
-              ),
-            );
-          },
-        );
-      },
-    );
-  }
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class ProductPage extends StatelessWidget {
-  ProductPage({Key? key, required this.item}) : super(key: key);
-  final Product item;
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(this.item.name),
+        title: Text(CustomLocalizations.of(context)!.title),
       ),
       body: Center(
-        child: Container(
-          padding: EdgeInsets.all(0),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Image.asset("assets/appimages/" + this.item.image),
-                Expanded(
-                    child: Container(
-                        padding: EdgeInsets.all(5),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Text(this.item.name,
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text(this.item.description),
-                            Text("Price: " + this.item.price.toString()),
-                            RatingBox(),
-                          ],
-                        )))
-              ]),
+        child: Text(
+          CustomLocalizations.of(context)!.message,
+          style: Theme.of(context).textTheme.headline5,
         ),
       ),
     );
   }
 }
 
-class RatingBox extends StatefulWidget {
-  @override
-  _RatingBoxState createState() => _RatingBoxState();
-}
+class CustomLocalizations {
+  static Future<CustomLocalizations> load(Locale locale) {
+    final String name =
+    locale.countryCode!.isEmpty ? locale.languageCode : locale.toString();
+    final String localeName = Intl.canonicalizedLocale(name);
 
-class _RatingBoxState extends State<RatingBox> {
-  int _rating = 0;
-  void _setRatingAsZero() {
-    setState(() {
-      _rating = 0;
+    return initializeMessages(localeName).then((_) {
+      Intl.defaultLocale = localeName;
+      return CustomLocalizations();
     });
   }
 
-  void _setRatingAsOne() {
-    setState(() {
-      _rating = 1;
-    });
+  static CustomLocalizations? of(BuildContext context) {
+    return Localizations.of<CustomLocalizations>(context, CustomLocalizations);
   }
 
-  void _setRatingAsTwo() {
-    setState(() {
-      _rating = 2;
-    });
+  String get title {
+    return Intl.message(
+      'Lesson 19',
+      name: 'title',
+      desc: 'Title for the Lesson19 application',
+    );
   }
 
-  void _setRatingAsThree() {
-    setState(() {
-      _rating = 3;
-    });
-  }
-
-  Widget build(BuildContext context) {
-    double _size = 20;
-    print(_rating);
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      mainAxisSize: MainAxisSize.max,
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(0),
-          child: IconButton(
-            icon: (_rating >= 1
-                ? Icon(
-              Icons.star,
-              size: _size,
-            )
-                : Icon(
-              Icons.star_border,
-              size: _size,
-            )),
-            color: Colors.red[500],
-            onPressed: _setRatingAsOne,
-            iconSize: _size,
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.all(0),
-          child: IconButton(
-            icon: (_rating >= 2
-                ? Icon(
-              Icons.star,
-              size: _size,
-            )
-                : Icon(
-              Icons.star_border,
-              size: _size,
-            )),
-            color: Colors.red[500],
-            onPressed: _setRatingAsTwo,
-            iconSize: _size,
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.all(0),
-          child: IconButton(
-            icon: (_rating >= 3
-                ? Icon(
-              Icons.star,
-              size: _size,
-            )
-                : Icon(
-              Icons.star_border,
-              size: _size,
-            )),
-            color: Colors.red[500],
-            onPressed: _setRatingAsThree,
-            iconSize: _size,
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.all(0),
-          child: GestureDetector(
-              onTap: () {
-                _setRatingAsZero();
-              },
-              child: Text( 'Clear', )
-          ),
-        ),
-      ],
+  String get message {
+    return Intl.message(
+      'Hello World',
+      name: 'message',
+      desc: 'Message for the Lesson19 application',
     );
   }
 }
 
-class ProductBox extends StatelessWidget {
-  ProductBox({Key? key, required this.item}) : super(key: key);
-  final Product item;
+class CustomLocalizationsDelegate
+    extends LocalizationsDelegate<CustomLocalizations> {
+  const CustomLocalizationsDelegate();
 
-  Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.all(2),
-        height: 140,
-        child: Card(
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Image.asset("assets/appimages/" + this.item.image),
-                Expanded(
-                    child: Container(
-                        padding: EdgeInsets.all(5),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Text(this.item.name,
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text(this.item.description),
-                            Text("Price: " + this.item.price.toString()),
-                            RatingBox(),
-                          ],
-                        )))
-              ]),
-        ));
+  @override
+  bool isSupported(Locale locale) => ['en', 'vi'].contains(locale.languageCode);
+
+  @override
+  Future<CustomLocalizations> load(Locale locale) {
+    return CustomLocalizations.load(locale);
   }
+
+  @override
+  bool shouldReload(CustomLocalizationsDelegate old) => false;
 }
